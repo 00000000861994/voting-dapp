e06fd46c80909ac54d39bf41ef231752352a60e5
dubey <pradipti.dubey@capgemini.com>
2024-01-05 21:10:25 +0530

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,13 +8,13 @@ require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-   solidity: "0.8.11",
-   defaultNetwork: "volta",
+   solidity: "0.8.0",
+   defaultNetwork: "Goerli",
    networks: {
       hardhat: {},
-      volta: {
+      Goerli: {
          url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`],
+         accounts: [`0xc5e742eb1b151a01b064fdafc6d9794b118081daeb2dadf9018377eb5d6d865d`],
          gas: 210000000,
          gasPrice: 800000000000,
       }
